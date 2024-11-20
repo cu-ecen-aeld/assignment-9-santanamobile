737f0ef8feaa78086bcee37b7883acb59a8d77a8
Helder Santana <3494526+santanamobile@users.noreply.github.com>
2024-11-20 20:21:50 -0300

    Assignment #8 P2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '6bc7193aa798769ee4d51fe74c3dcb511122a162'
+AESD_ASSIGNMENTS_VERSION = 'a82245147522be2060c291c3b4b76dad1ca94ea6'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesd-chardriver/aesd-chardriver.mk
+++ base_external/package/aesd-chardriver/aesd-chardriver.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-AESD_CHARDRIVER_VERSION = '6bc7193aa798769ee4d51fe74c3dcb511122a162'
+AESD_CHARDRIVER_VERSION = 'a82245147522be2060c291c3b4b76dad1ca94ea6'
 AESD_CHARDRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-santanamobile.git'
 AESD_CHARDRIVER_SITE_METHOD = git
 AESD_CHARDRIVER_GIT_SUBMODULES = YES
